74b354fd54a4ae52704da95d9d6900a7ee5aeff2
SEKINE, Hideki <sekineh@me.com>
2018-03-08 19:13:30 +0900

    folked from Rust std as of 1.24.1.

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+mod binary_heap;
+pub use binary_heap::BinaryHeap;
+extern crate core;
+
+/// An intermediate trait for specialization of `Extend`.
+// #[doc(hidden)]
+// trait SpecExtend<I: IntoIterator> {
+//     /// Extends `self` with the contents of the given iterator.
+//     fn spec_extend(&mut self, iter: I);
+// }
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
